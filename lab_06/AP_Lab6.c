94c24b8d6d7c3acf0372a5a3b4b9ba07c8245bc9
edmundoferreira <fc.edmundo@gmail.com>
2016-11-24 16:27:49 +0100

    - finished lab 6 - implemented using OS using lab 3 OS

--- lab_06/AP_Lab6.c
+++ lab_06/AP_Lab6.c
@@ -5,7 +5,7 @@
 // Daniel Valvano and Jonathan Valvano
 // September 18, 2016
 // CC2650 booster or CC2650 LaunchPad, CC2650 needs to be running SimpleNP 2.2 (POWERSAVE)
-
+#include <string.h>
 #include <stdint.h>
 #include "../inc/UART0.h"
 #include "../inc/UART1.h"
@@ -61,6 +61,13 @@ extern NotifyCharacteristic_t NotifyCharacteristicList[];
 void SetFCS(uint8_t *msg){
 //****You implement this function as part of Lab 6*****
 
+	uint8_t fcs = 0;
+	uint32_t i;
+
+	for(i=1; i< (msg[1]+5); i++)
+		fcs = fcs ^ msg[i];
+	
+	msg[i] = fcs;
   
 }
 //*************BuildGetStatusMsg**************
@@ -72,7 +79,13 @@ void BuildGetStatusMsg(uint8_t *msg){
 // hint: see NPI_GetStatus in AP.c
 //****You implement this function as part of Lab 6*****
 
-  
+	msg[0] = SOF;
+	msg[1] = 0x00; //lenght
+	msg[2] = 0x00; 
+	msg[3] = 0x55; //cmd0
+	msg[4] = 0x06; //cmd1
+
+	SetFCS(msg); //expected 0x53
 }
 //*************Lab6_GetStatus**************
 // Get status of connection, used in Lab 6
@@ -98,7 +111,14 @@ void BuildGetVersionMsg(uint8_t *msg){
 // hint: see NPI_GetVersion in AP.c
 //****You implement this function as part of Lab 6*****
   
-  
+  msg[0] = SOF;
+	msg[1] = 0x00; //lenght
+	msg[2] = 0x00;
+	msg[3] = 0x35; //cmd0
+	msg[4] = 0x03; //cmd1
+
+	SetFCS(msg); //expected 0x36
+	
 }
 //*************Lab6_GetVersion**************
 // Get version of the SNP application running on the CC2650, used in Lab 6
@@ -119,7 +139,15 @@ uint32_t Lab6_GetVersion(void){volatile int r;uint8_t sendMsg[8];
 // build the necessary NPI message that will add a service
 void BuildAddServiceMsg(uint16_t uuid, uint8_t *msg){
 //****You implement this function as part of Lab 6*****
-  
+  msg[0] = SOF;
+	msg[1] = 3;		 //lenght
+	msg[2] = 0x00;
+	msg[3] = 0x35; //cmd0
+	msg[4] = 0x81; //cmd1 
+	msg[5] = 0x01; //Type(1byte) => (Primary Service, Secondary Service) = (0x01, 0x02)
+	memcpy(&msg[6], &uuid, sizeof(uuid)); //UUID(16bytes)
+		
+	SetFCS(msg);
   
 }
 //*************Lab6_AddService**************
@@ -140,8 +168,14 @@ int Lab6_AddService(uint16_t uuid){ int r; uint8_t sendMsg[12];
 // build the necessary NPI message that will register a service
 void BuildRegisterServiceMsg(uint8_t *msg){
 //****You implement this function as part of Lab 6*****
-  
-  
+ 
+	msg[0] = SOF;
+	msg[1] = 0x00;	//lenght
+	msg[2] = 0x00;  
+	msg[3] = 0x35;  //cmd0
+	msg[4] = 0x84;  //cmd1 
+	
+	SetFCS(msg); //expected 0x00? 0xB1
 }
 //*************Lab6_RegisterService**************
 // Register a service, used in Lab 6
@@ -170,8 +204,21 @@ void BuildAddCharValueMsg(uint16_t uuid,
 // for a hint see NPI_AddCharValue in AP.c
 // for a hint see first half of AP_AddCharacteristic and first half of AP_AddNotifyCharacteristic
 //****You implement this function as part of Lab 6*****
-  
-    
+
+		msg[0] = SOF;		
+		msg[1] = 8;		 //lenght
+		msg[2] = 0x00; 
+    msg[3] = 0x35; //cmd0
+		msg[4] = 0x82; //cmd1 
+		msg[5] = permission; //Value Permissions(1byte) => (R,W,RW)=(0x1, 0x2, 0x3)
+		msg[6] = properties; //Value Properties(2bytes)
+		msg[7] = 0x00;
+		msg[8] = 0x00; // RFU(1byte)
+		msg[9] = 0x00; // Value Max Length(2bytes)
+		msg[10] = 0x02;
+		memcpy(&msg[11], &uuid, sizeof(uuid)); //UUID(16bytes)
+		
+		SetFCS(msg);
 }
 
 //*************BuildAddCharDescriptorMsg**************
@@ -186,8 +233,22 @@ void BuildAddCharDescriptorMsg(char name[], uint8_t *msg){
 // for a hint see NPI_AddCharDescriptor in AP.c
 // for a hint see second half of AP_AddCharacteristic
 //****You implement this function as part of Lab 6*****
-  
-  
+  uint8_t name_size = strlen(name)+1;
+	
+	msg[0] = SOF;
+	msg[1] = 6+name_size; 	 //lenght
+	msg[2] = 0x00;  
+	msg[3] = 0x35; //cmd0
+	msg[4] = 0x83; //cmd1
+	msg[5] = 0x80; //Header(1byte) => 0x80 User Description String
+	msg[6] = 0x01; //Permissions(1byte): GATT permissions of the User Description Attribute => (R,W,RW)=(0x1, 0x2, 0x3)
+	msg[7] = name_size;	 //Max Length(2bytes)
+	msg[8] = 0x00; 
+	msg[9] = name_size; //Initial Length (2 bytes)
+	msg[10]= 0x00; 
+	memcpy(&msg[11], name, name_size); //Description(1-512bytes)
+	
+	SetFCS(msg);
 }
 
 //*************Lab6_AddCharacteristic**************
@@ -243,8 +304,23 @@ void BuildAddNotifyCharDescriptorMsg(char name[], uint8_t *msg){
 // for a hint see NPI_AddCharDescriptor4 in VerySimpleApplicationProcessor.c
 // for a hint see second half of AP_AddNotifyCharacteristic
 //****You implement this function as part of Lab 6*****
-  
-  
+  uint8_t name_size = strlen(name)+1;
+	
+	msg[0] = SOF;
+	msg[1] = 7+name_size; 	 //lenght
+	msg[2] = 0x00;  
+	msg[3] = 0x35; //cmd0
+	msg[4] = 0x83; //cmd1
+	msg[5] = 0x84; //Header(1byte) => 0x84 User Description String, and CCCD permissions
+	msg[6] = 0x03; //Permissions(1byte): GATT permissions of CCCD attribute) =>  (R,W,RW)=(0x1, 0x2, 0x3)
+	msg[7] = 0x01; //Permissions(1byte): GATT permissions of the User Description Attribute => (R,W,RW)=(0x1, 0x2, 0x3)
+	msg[8] = name_size;	 //Max Length(2bytes)
+	msg[9] = 0x00; 
+	msg[10]= name_size; //Initial Length (2 bytes)
+	msg[11]= 0x00; 
+	memcpy(&msg[12], name, name_size); //Description(1-512bytes)
+	
+	SetFCS(msg);
 }
   
 //*************Lab6_AddNotifyCharacteristic**************
@@ -293,8 +369,19 @@ void BuildSetDeviceNameMsg(char name[], uint8_t *msg){
 // for a hint see NPI_GATTSetDeviceNameMsg in VerySimpleApplicationProcessor.c
 // for a hint see NPI_GATTSetDeviceName in AP.c
 //****You implement this function as part of Lab 6*****
-  
-  
+  uint8_t name_size = strlen(name);
+	
+	msg[0] = SOF;					
+	msg[1] = 3+name_size; //lenght
+	msg[2] = 0x00;        
+	msg[3] = 0x35; //cmd0
+	msg[4] = 0x8c; //cmd1 
+	msg[5] = 0x01; //Service ID (1byte) => Generic Access Service(0x01)
+	msg[6] = 0x00; //Parameter ID (2 bytes) => Device Name (0x00)
+	msg[7] = 0x00;
+	memcpy(&msg[8], name, name_size);
+	
+	SetFCS(msg);
 }
 //*************BuildSetAdvertisementData1Msg**************
 // Create a Set Advertisement Data message, used in Lab 6
@@ -311,8 +398,25 @@ void BuildSetAdvertisementData1Msg(uint8_t *msg){
 // TI_ST_KEY_DATA_ID
 // Key state=0
 //****You implement this function as part of Lab 6*****
-  
-  
+    
+	msg[0] = SOF;
+  msg[1] = 11;   //lenght
+	msg[2] = 0x00;              
+	msg[3] = 0x55; //cmd0
+	msg[4] = 0x43; //cmd1
+	msg[5] = 0x01; //Advertisement Type(1byte) => Non-connectable Advertisement Data (0x01)
+	msg[6] = 0x02; //length of this field
+	msg[7] = 0x01; //advertising type flags
+	msg[8] = 0x06; //general discovery, no BREDR
+	msg[9] = 0x06; //length
+	msg[10]= 0xFF; //manufacturer specific
+	msg[11]= 0x0D; //Texas Instruments Company ID
+	msg[12]= 0x00;
+	msg[13]= 0x03; //TI_ST_DEVICE_ID
+  msg[14]= 0x00; //TI_ST_KEY_DATA_ID
+  msg[15]= 0x00; //Key state
+	
+	SetFCS(msg); //expected FCS 0xEE
 }
 
 //*************BuildSetAdvertisementDataMsg**************
@@ -325,8 +429,29 @@ void BuildSetAdvertisementDataMsg(char name[], uint8_t *msg){
 // for a hint see NPI_SetAdvertisementDataMsg in VerySimpleApplicationProcessor.c
 // for a hint see NPI_SetAdvertisementData in AP.c
 //****You implement this function as part of Lab 6*****
-  
-  
+	
+	uint8_t name_size = strlen(name);
+	
+	msg[0] = SOF;
+	msg[1] = 12 + name_size; //length
+	msg[2] = 0x00;              
+	msg[3] = 0x55; //cmd0
+	msg[4] = 0x43; //cmd1
+	msg[5] = 0x00; //Scan Response Data
+  msg[6] = name_size+1; //length,
+	msg[7] = 0x09; //type=LOCAL_NAME_COMPLETE
+  memcpy(&msg[8], name, name_size);
+	msg[8+name_size+0] = 0x05; //length of this data
+  msg[8+name_size+1] = 0x12; //GAP_ADTYPE_SLAVE_CONN_INTERVAL_RANGE
+  msg[8+name_size+2] = 0x50; //DEFAULT_DESIRED_MIN_CONN_INTERVAL
+	msg[8+name_size+3] = 0x00;      
+  msg[8+name_size+4] = 0x20;
+	msg[8+name_size+5] = 0x03; //DEFAULT_DESIRED_MAX_CONN_INTERVAL
+  msg[8+name_size+6] = 0x02; //length of this data
+  msg[8+name_size+7] = 0x0A; //GAP_ADTYPE_POWER_LEVEL
+  msg[8+name_size+8] = 0x00; //0dBm
+	
+	SetFCS(msg);
 }
 //*************BuildStartAdvertisementMsg**************
 // Create a Start Advertisement Data message, used in Lab 6
@@ -338,8 +463,27 @@ void BuildStartAdvertisementMsg(uint16_t interval, uint8_t *msg){
 // for a hint see NPI_StartAdvertisementMsg in VerySimpleApplicationProcessor.c
 // for a hint see NPI_StartAdvertisement in AP.c
 //****You implement this function as part of Lab 6*****
+
+	msg[0] = SOF;
+	msg[1] = 14; 	 //lenght
+	msg[2] = 0x00;
+	msg[3] = 0x55; //cmd0
+	msg[4] = 0x42; //cmd1
+	msg[5] = 0x00; //Advertisement Type(1byte) => Connectable Undirected Advertisements
+  msg[6] = 0x00; //Timeout(2bytes) => Advertise infinitely.
+	msg[7] = 0x00; 
+	memcpy(&msg[8], &interval, sizeof(interval));//Interval(2bytes) => Advertising Interval (interval* 0.625 ms)
+	msg[10]= 0x00; //Filter Policy(1byte) => RFU
+  msg[11]= 0x00; //Initiator Address Type(1byte) => RFU
+  msg[12]= 0x00; //Initiator Address(6bytes)
+	msg[13]= 0x01; 
+	msg[14]= 0x00;
+	msg[15]= 0x00;
+	msg[16]= 0x00;
+	msg[17]= 0xC5; 
+  msg[18]= 0x02; //Behavior (1 byte) =>  Advertising will restart with connectable advertising when a connection is terminated
   
-  
+	SetFCS(msg); // expectec FCS 0xBB 
 }
 
 //*************Lab6_StartAdvertisement**************
